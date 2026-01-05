f69083ea0a83a157d49ae80453a1fadc4ff9fe01
diegoasp360 <diego.saavedra@blend360.com>
2026-01-05 11:57:59 -0500

    Initial commit

---
+++ test/aws_devops_agent.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsDevopsAgent from '../lib/aws_devops_agent-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws_devops_agent-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsDevopsAgent.AwsDevopsAgentStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
